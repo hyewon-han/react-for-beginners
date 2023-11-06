2a715f16de89cb9b8bda9882fd059511ebe43cba
hyewon-han <hw1635@hanmail.net>
2023-11-06 18:50:38 +0900

    update css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import styles from "./Detail.module.css";
 
 function Detail() {
   const { id } = useParams();
@@ -19,10 +20,33 @@ function Detail() {
   return (
     <div>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
-          <img src={movie.large_cover_image} />
+        <div className={styles.movie__wrap}>
+          <img
+            src={movie.large_cover_image}
+            className={styles.movie__coverimg}
+          />
+          <img src={movie.background_image} className={styles.movie__bgimg} />
+          <div className={styles.movie__info}>
+            <p className={styles.movie__title}>{movie.title}</p>
+            <ul className={styles.movie__genres}>
+              {movie.genres.map((item, idx) => (
+                <li key={idx}>{item}</li>
+              ))}
+            </ul>
+            <p>summary : {movie.description_full}</p>
+            <p>runtime : {movie.runtime}</p>
+            <p>year : {movie.year}</p>
+            <a
+              href={`https://www.youtube.com/watch?v=${movie.yt_trailer_code}`}
+              target="_blank"
+            >
+              Go to youtube
+            </a>
+          </div>
         </div>
       )}
     </div>
