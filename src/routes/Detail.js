f79e6214180be352ccc28e61174b5988eda97f1a
hyewon-han <hw1635@hanmail.net>
2023-11-06 15:13:11 +0900

    first commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function Detail() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.large_cover_image} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
